9e561d6447fbd71bd5f90317a551b5c5fdb72bda
Viacheslav Tsarenkov <tsarenkovslava@yandex.ru>
2020-10-02 11:36:37 +0300

    feat
    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+
+  if(array === undefined || array.length === 0)
   return 0;
+
+  let min = parseInt(array[0]);
+  for (let i = 1; i < array.length; ++i){
+    if (parseInt(array[i]) < min)
+      min = parseInt(array[i]);
+  }
+  return min;
 }
 
 exports.max = function max (array) {
+  if(array === undefined || array.length === 0)
   return 0;
+  let max = parseFloat(array[0]);
+  for (let i = 1; i < array.length; ++i){
+    if (parseFloat(array[i]) > max)
+      max = parseFloat(array[i]);
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
+  if(array === undefined || array.length === 0)
   return 0;
+  let avg= 0;
+  for (let i = 0; i < array.length; ++i){
+      avg += parseFloat(array[i]);
+  }
+  return avg / array.length;
 }
